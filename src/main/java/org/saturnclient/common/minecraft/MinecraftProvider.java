5bb69c92eb71b159ce6329359c17a22f619fc15f
Klesti Selimaj <klesti.selimay@icloud.com>
2026-03-06 11:11:24 +0100

    Adding more implementations and fixes

--- src/main/java/org/saturnclient/common/minecraft/MinecraftProvider.java
+++ src/main/java/org/saturnclient/common/minecraft/MinecraftProvider.java
@@ -1,13 +1,18 @@
 package org.saturnclient.common.minecraft;
 
-import java.io.File;
+import org.saturnclient.common.minecraft.bindings.SaturnIdentifier;
+import java.awt.image.BufferedImage;
 
 public abstract class MinecraftProvider {
     public static MinecraftProvider PROVIDER;
+
+    public abstract IMinecraftClient getClient();
 
     public abstract Object createIdentifier(String namespace, String path);
 
     public abstract boolean isKeyPressed(int key);
 
-    public abstract File getRunDirectory();
+    public abstract int getWidth(String text, int font);
+
+    public abstract void registerBufferedImageTexture(SaturnIdentifier i, BufferedImage bi);
 }
--- src/main/java/org/saturnclient/common/minecraft/bindings/SaturnIdentifier.java
+++ src/main/java/org/saturnclient/common/minecraft/bindings/SaturnIdentifier.java
@@ -7,4 +7,18 @@ public class SaturnIdentifier extends MinecraftBinding {
     public SaturnIdentifier(String namespace, String path) {
         super(MinecraftProvider.PROVIDER.createIdentifier(namespace, path));
     }
+
+    public static SaturnIdentifier of(String namespace, String path) {
+        return new SaturnIdentifier(namespace, path);
+    }
+
+    public static SaturnIdentifier of(String fullPath) {
+        String[] p = fullPath.split(":", 2);
+
+        return new SaturnIdentifier(p[0], p[1]);
+    }
+
+    public static SaturnIdentifier ofVanilla(String path) {
+        return new SaturnIdentifier("minecraft", path);
+    }
 }
