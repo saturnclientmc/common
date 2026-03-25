fc1500abbdf911987ee3d98ee2015bec7cbc96eb
Klesti Selimaj <klesti.selimay@icloud.com>
2026-03-25 18:43:35 +0100

    Improved MatrixStackRef #59

--- src/main/java/org/saturnclient/common/ref/render/MatrixStackRef.java
+++ src/main/java/org/saturnclient/common/ref/render/MatrixStackRef.java
@@ -1,11 +1,15 @@
 package org.saturnclient.common.ref.render;
 
 public interface MatrixStackRef {
+    public void push();
+
+    public void pop();
+
     public void translate(float x, float y);
 
     public void scale(float x, float y);
 
-    public void push();
+    public void rotate(float angle, float originX, float originY, float originZ);
 
-    public void pop();
+    public void rotate(float angle);
 }
